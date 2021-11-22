594b1e55ce9ad8a9a88ee2a3f20ecee7342487e9
Josef Brandt <josef.brandt@gu.se>
2021-11-22 11:48:11 +0100

    Updated resnet clf

--- classification/classificationExperiments.py
+++ classification/classificationExperiments.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 import imblearn
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
@@ -32,11 +32,11 @@ for i in range(len(assignments)):
 assignments[assignments == "sediment A"] = "Sediment"
 assignments[assignments == "sediment B"] = "Sediment"
 assignments[assignments == "sediment C"] = "Sediment"
-print(Counter(assignments))
+indNoAmber: np.ndarray = np.where(assignments != "Amber")[0]
+specs = specs[indNoAmber, :]
+assignments = assignments[indNoAmber]
+print("raw", Counter(assignments))
 
-
-specs, assignments = imblearn.under_sampling.RandomUnderSampler().fit_resample(specs, assignments)
-print(Counter(assignments))
 
 specs = deriv_smooth(specs, windowSize=13, polydegree=3, derivative=1)
 specs = normalizeIntensities(specs, NormMode.Length)
@@ -45,14 +45,18 @@ encoder: LabelEncoder = LabelEncoder().fit(assignments)
 y: np.ndarray = encoder.transform(assignments)
 
 X_train, X_test, y_train, y_test = train_test_split(specs, y, test_size=0.2)
+X_train, y_train = imblearn.over_sampling.SMOTE().fit_resample(X_train, y_train)
+X_test, y_test = imblearn.under_sampling.RandomUnderSampler().fit_resample(X_test, y_test)
+print("balanced train:", Counter(y_train))
+print("balanced test:", Counter(y_test))
+
 dropout = 0.1
 numNeurons = [200, 100, 50]
 
-modelName: str = f"Dense {'_'.join([str(i) for i in numNeurons])}_dropout_{dropout}_20epochs"
+modelName: str = "DenseNet_with_Preproc"
 print(modelName)
 
 numFeatures, numClasses = specs.shape[1], len(np.unique(assignments))
-
 nn: NeuralNetClf = NeuralNetClf(numFeatures, numClasses,
                                 numNeuronsPerHiddenLayer=numNeurons, dropout=dropout)
 tensboard: TensorBoard = TensorBoard(log_dir=f"logs/{modelName}")
@@ -60,7 +64,7 @@ t0 = time.time()
 history = nn.fit(X_train, to_categorical(y_train),
                  validation_data=(X_test, to_categorical(y_test)),
                  batch_size=64,
-                 epochs=5, verbose=1,
+                 epochs=50, verbose=1,
                  callbacks=[tensboard])
 print(f"training NN took {time.time()-t0} seconds")
 t0 = time.time()
@@ -71,16 +75,19 @@ print(f"Pred NN: {time.time()-t0} seconds\n", classification_report(encoder.inve
                                                                     zero_division=0))
 
 
-modelName = "ResNet1D"
+modelName = "ResNet1D_with_Preproc"
 X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
 X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
-resnet: ResNet1D = ResNet1D(numFeatures, numClasses)
+resnet: ResNet1D = ResNet1D(numFeatures, numClasses, n_blocks=6, n_layers=4, kSize=3)
+tensboard: TensorBoard = TensorBoard(log_dir=f"logs/{modelName}")
+modelCheckpoint: ModelCheckpoint = ModelCheckpoint("ResNetModelSavesWithPreprocess", monitor="val_accuracy")
+# resnet.summary()
 t0 = time.time()
 history = resnet.fit(X_train, to_categorical(y_train),
                      validation_data=(X_test, to_categorical(y_test)),
                      batch_size=64,
-                     epochs=5, verbose=1,
-                     callbacks=[tensboard])
+                     epochs=100, verbose=1,
+                     callbacks=[tensboard, modelCheckpoint])
 print(f"training Resnet took {time.time()-t0} seconds")
 t0 = time.time()
 pred: np.ndarray = resnet.predict(X_test)
@@ -88,7 +95,6 @@ pred = np.array([pred[i, :].argmax() for i in range(pred.shape[0])])
 print(f"Pred Resnet: {time.time()-t0} seconds\n", classification_report(encoder.inverse_transform(y_test),
                                                                     encoder.inverse_transform(pred),
                                                                     zero_division=0))
-
 
 
 # modelName: str = f"Conv Network_20epochs"
--- classification/neuralNetModels/resNet.py
+++ classification/neuralNetModels/resNet.py
@@ -28,85 +28,44 @@ from tensorflow.keras.layers import Activation, add, Conv1D, GlobalAveragePoolin
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import Precision, Recall
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 
 class ResNet1D(Model):
-    def __init__(self, numFeatures: int, numClasses: int):
-        n_feature_maps = 64
+    def __init__(self, numFeatures: int, numClasses: int, n_blocks: int, n_layers: int, kSize: int):
+        n_filters = 64
 
         input_layer = Input((numFeatures, 1))
-
-        # BLOCK 1
-        conv_x = Conv1D(filters=n_feature_maps, kernel_size=8, padding='same')(input_layer)
-        conv_x = BatchNormalization()(conv_x)
-        conv_x = Activation('relu')(conv_x)
-
-        conv_y = Conv1D(filters=n_feature_maps, kernel_size=5, padding='same')(conv_x)
-        conv_y = BatchNormalization()(conv_y)
-        conv_y = Activation('relu')(conv_y)
-
-        conv_z = Conv1D(filters=n_feature_maps, kernel_size=3, padding='same')(conv_y)
-        conv_z = BatchNormalization()(conv_z)
-
-        # expand channels for the sum
-        shortcut_y = Conv1D(filters=n_feature_maps, kernel_size=1, padding='same')(input_layer)
-        shortcut_y = BatchNormalization()(shortcut_y)
-
-        output_block_1 = add([shortcut_y, conv_z])
-        output_block_1 = Activation('relu')(output_block_1)
-
-        # BLOCK 2
-        conv_x = Conv1D(filters=n_feature_maps * 2, kernel_size=8, padding='same')(output_block_1)
-        conv_x = BatchNormalization()(conv_x)
-        conv_x = Activation('relu')(conv_x)
-
-        conv_y = Conv1D(filters=n_feature_maps * 2, kernel_size=5, padding='same')(conv_x)
-        conv_y = BatchNormalization()(conv_y)
-        conv_y = Activation('relu')(conv_y)
-
-        conv_z = Conv1D(filters=n_feature_maps * 2, kernel_size=3, padding='same')(conv_y)
-        conv_z = BatchNormalization()(conv_z)
-
-        # expand channels for the sum
-        shortcut_y = Conv1D(filters=n_feature_maps * 2, kernel_size=1, padding='same')(output_block_1)
-        shortcut_y = BatchNormalization()(shortcut_y)
-
-        output_block_2 = add([shortcut_y, conv_z])
-        output_block_2 = Activation('relu')(output_block_2)
-
-        # BLOCK 3
-        conv_x = Conv1D(filters=n_feature_maps * 2, kernel_size=8, padding='same')(output_block_2)
-        conv_x = BatchNormalization()(conv_x)
-        conv_x = Activation('relu')(conv_x)
-
-        conv_y = Conv1D(filters=n_feature_maps * 2, kernel_size=5, padding='same')(conv_x)
-        conv_y = BatchNormalization()(conv_y)
-        conv_y = Activation('relu')(conv_y)
-
-        conv_z = Conv1D(filters=n_feature_maps * 2, kernel_size=3, padding='same')(conv_y)
-        conv_z = BatchNormalization()(conv_z)
-
-        # no need to expand channels because they are equal
-        shortcut_y = BatchNormalization()(output_block_2)
-
-        output_block_3 = add([shortcut_y, conv_z])
-        output_block_3 = Activation('relu')(output_block_3)
+        for i in range(n_blocks):
+            if i == 0:
+                output_block = self._addResBlock(32, n_layers, kSize, input_layer)
+            else:
+                output_block = self._addResBlock(n_filters, n_layers, kSize, output_block)
 
         # FINAL
-        gap_layer = GlobalAveragePooling1D()(output_block_3)
+        gap_layer = GlobalAveragePooling1D()(output_block)
         output_layer = Dense(numClasses, activation='softmax')(gap_layer)
 
         super(ResNet1D, self).__init__(inputs=input_layer, outputs=output_layer)
 
         self.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=[Precision(), Recall()])
-        #
-        # reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=50, min_lr=0.0001)
-        #
-        # file_path = self.output_directory + 'best_model.hdf5'
-        #
-        # model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='loss',
-        #                                                    save_best_only=True)
-        #
-        # self.callbacks = [reduce_lr, model_checkpoint]
 
-        # return model
+    def _addResBlock(self, n_filters: int, kSize: int, n_layers: int, input_layer):
+        for i in range(n_layers):
+            if i == 0:
+                conv = Conv1D(filters=n_filters, kernel_size=kSize, padding='same')(input_layer)
+            else:
+                conv = Conv1D(filters=n_filters, kernel_size=kSize, padding='same')(conv)
+            conv = BatchNormalization()(conv)
+            conv = Activation('relu')(conv)
+
+        # expand channels for the sum
+        if input_layer.shape[2] != n_filters:
+            shortcut = Conv1D(filters=n_filters, kernel_size=1, padding='same')(input_layer)
+            shortcut = BatchNormalization()(shortcut)
+        else:
+            shortcut = BatchNormalization()(input_layer)
+
+        outputLayer = add([shortcut, conv])
+        outputLayer = Activation('relu')(outputLayer)
+        return outputLayer
